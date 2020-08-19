439d51b0a9fd573376622815326ae601aa077c1d
acnug1 <acnug0gyBaH4uK@yandex.ru>
2020-08-19 14:31:14 +0500

    Add folder scripts

---
+++ Scripts/VolumeChanger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class VolumeChanger : MonoBehaviour
+{
+    [SerializeField] private AudioSource _audio;
+
+    public void StartVolumeChanger(bool onEnable)
+    {
+        StopAllCoroutines();
+
+        StartCoroutine(ChangeIn(onEnable));
+    }
+
+    private IEnumerator ChangeIn(bool onEnabled)
+    {
+        var waitForEndOfFrame = new WaitForEndOfFrame();
+
+        if (onEnabled)
+        {
+            _audio.Play();
+
+            for (int i = 0; i < 101; i++)
+            {
+                _audio.volume = i * 1f / 100f;
+
+                yield return waitForEndOfFrame;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < 101; i++)
+            {
+                _audio.volume = 1f - (i * 1f / 100f);
+
+                yield return waitForEndOfFrame;
+            }
+
+            _audio.Stop();
+        }
+    }
+}
