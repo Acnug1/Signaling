21f420ccb9702e8bc8bae3fc8900098f6aa06cf0
acnug1 <acnug0gyBaH4uK@yandex.ru>
2021-11-09 19:17:04 +0500

    Init unity project

---
+++ Assets/Scripts/Signaling.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(VolumeChanger))]
+
+public class Signaling : MonoBehaviour
+{
+    [SerializeField] private UnityEvent _reached;
+
+    private VolumeChanger _volumeChanger;
+
+    private void Awake()
+    {
+        _volumeChanger = GetComponent<VolumeChanger>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            _reached?.Invoke();
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.TryGetComponent<Player>(out Player player))
+        {
+            _volumeChanger.StartVolumeChanger(false);
+        }
+    }
+}
